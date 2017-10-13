4a48b15af96b69f7d235f46be9dd0e35cc6d9f85
Azer7 <Azer7@users.noreply.github.com>
2017-10-12 20:57:11 -0600

    start

---
+++ main.js
@@ -0,0 +1,33 @@
+let neuron;
+
+let guess;
+let points = [];
+
+let learningRate = 
+
+function setup() {
+	createCanvas(500, 500);
+	background("white");
+
+	neuron = new Neuron();
+
+	for (let i = 0; i < 100; i++) {
+	    points.push(new Point);
+	}
+
+	//console.log(output);
+}
+
+function draw() {
+    background(255);
+    stroke(0);
+    line(0, 0, width, height);
+    for (let i = 0; i < points.length; i++) {
+        points[i].show();
+    }
+
+    for (let i = 0; i < points.length; i++) {
+        let pInputs = [points[i].x, points[i].y];
+        neuron.train(pInputs, points[i].label);
+    }
+}
---
+++ neuron.js
@@ -0,0 +1,36 @@
+﻿class Neuron {
+    constructor() {
+        this.weights = [];
+        for (let i = 0; i < 2; i++) {
+            this.weights.push(random(-1, 1));
+        }
+
+        this.lr = 0.1;
+    }
+
+    guess(inputs) { //create a guess based on the inputs
+        let sum = 0;
+
+        for (let i = 0; i < this.weights.length; i++) {
+            sum += inputs[i] * this.weights[i];
+        }
+        return sign(sum);
+    }
+
+    train(inputs, target) {
+        let guess = this.guess(inputs); //make a guess based on the inputs point:(x,y)
+        let error = target - guess;
+
+        //change the weights
+        for (let i = 0; i < this.weights.length; i++) {
+            this.weights[i] += error * inputs[i] * lr;
+        }
+    }
+}
+
+function sign(value) {
+    if (value >= 0)
+        return 1;
+    else
+        return -1;
+}
