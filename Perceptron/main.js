efa8797a9c7b69445fb597d6a38bf432c38d5331
Azer7 <Azer7@users.noreply.github.com>
2017-10-13 18:15:47 -0600

    moved around

---
+++ Perceptron/main.js
@@ -0,0 +1,72 @@
+let neuron;
+let points = [];
+
+let manual = true;
+let errorAmount = Infinity;
+
+function setup() {
+    createCanvas(500, 500);
+    background("white");
+
+    neuron = new Neuron(3);
+
+    for (let i = 0; i < 100; i++) {
+        points.push(new Point());
+    }
+}
+
+function draw() {
+    background(255);
+    stroke(0);
+    line(pixelX(-10), pixelY(f(-10)), pixelX(10), pixelY(f(10)));
+
+    line(pixelX(-10), pixelY(neuron.lineY(-10)), pixelX(10), pixelY(neuron.lineY(10)));
+
+    if(frameCount % 10 == 0 && errorAmount == 0) {
+        points.push(new Point());
+    }
+    
+    for (let i = 0; i < points.length; i++) {
+        points[i].show();
+    }
+
+    errorAmount = 0;
+    for (let i = 0; i < points.length; i++) {
+        let pInputs = [points[i].x, points[i].y, points[i].bias];
+        if (!manual)
+            neuron.train(pInputs, points[i].label);
+        let guess = neuron.guess(pInputs);
+        noFill();
+        strokeWeight(3);
+        if (guess == points[i].label)
+            stroke(0, 255, 0);
+        else {
+            errorAmount++;
+            stroke(255, 0, 0);
+        }
+        ellipse(points[i].pixelX(), points[i].pixelY(), 12, 12);
+    }
+}
+
+function pixelX(x) {
+    return map(x, -10, 10, 0, width);
+}
+
+function pixelY(y) {
+    return map(y, -10, 10, height, 0);
+}
+
+function mouseClicked() {
+    if (manual) {
+        for (let i = 0; i < points.length; i++) {
+            let pInputs = [points[i].x, points[i].y, points[i].bias];
+            neuron.train(pInputs, points[i].label);
+        }
+    }
+}
+
+function keyPressed() {
+    if(keyCode === 32) {
+        manual = !manual;
+    }
+}
