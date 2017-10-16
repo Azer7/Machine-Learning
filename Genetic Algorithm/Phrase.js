bf281f652aea1b209a47136901abada169b4cba2
Azer7 <Azer7@users.noreply.github.com>
2017-10-16 17:54:11 -0600

    infinite loop??

--- Genetic Algorithm/Phrase.js
+++ Genetic Algorithm/Phrase.js
@@ -1,32 +1,50 @@
 class Phrase {
     constructor(length) {
         this.chars = [];
+        this.length = length;
         this.fitness = 0;
-        for(let i = 0; i < length; i++) {
+        for (let i = 0; i < this.length; i++) {
             this.chars.push(randChar());
         }
     }
-    
+
     getWord() {
-        return this.chars.join("");   
+        return this.chars.join("");
     }
-    
-    calcFitness(target) {   
+
+    calcFitness(target) {
         let score = 0;
-        for(let i = 0; i < this.chars.length; i++) {
-            if(this.chars[i] == target.charAt(i))
+        for (let i = 0; i < this.chars.length; i++) {
+            if (this.chars[i] == target.charAt(i))
                 score++;
         }
         this.fitness = score / target.length;
-    }  
+    }
+
+    crossover(crossPhrase) {
+        let midpoint = random(0, this.length);
+        let result = new Phrase(this.length);
+        for (let i = 0; i < this.length; i++) {
+            if (i < midpoint) result.chars[i] = this.chars[i];
+            else result.chars[i] = crossPhrase[i];
+        }
+        return result;
+    }
+
+    mutate() {
+        for (let i = 0; i < this.length; i++) {
+            if (Math.random < mutationRate)
+                this.chars[i] = randChar();
+        }
+    }
 }
 
 function randChar() {
     let charCode = floor(random(63, 122));
-    if(charCode == 63)
+    if (charCode == 63)
         charCode = 32;
-    else if(charCode == 64)
+    else if (charCode == 64)
         charCode = 46;
-    
+
     return String.fromCharCode(charCode);
 }
--- Genetic Algorithm/Population.js
+++ Genetic Algorithm/Population.js
@@ -2,6 +2,7 @@ class Population {
     constructor(amount, mutationRate, target) {
         //variables
         this.population = [];
+        this.size = amount;
         this.target = target;
         this.mutationRate = mutationRate;
         //state
@@ -15,7 +16,7 @@ class Population {
     }
 
     processFitness() {
-        for (let i = 0; i < this.population.length; i++) {
+        for (let i = 0; i < this.size; i++) {
             this.population[i].calcFitness(this.target);
         }
     }
@@ -25,23 +26,32 @@ class Population {
         let maxFitIndex = 0;
         let totalFitness = 0;
 
-        for (let i = 0; i < this.population.length; i++) {
+        for (let i = 0; i < this.size; i++) {
             if (this.population[i].fitness > this.population[maxFitIndex].fitness) {
                 maxFitIndex = i;
             }
             totalFitness += this.population[i].fitness;
         }
-        let parentA = this.getRandomMember(totalFitness);
-        let parentB = this.getRandomMember(totalFitness);
-        
-        
+        futureGen.push(this.population[maxFitIndex]);
+        for (let i = 0; i < this.size - 1; i++) {
+            //get 2 random parents
+            let parentA = this.getRandomMember(totalFitness);
+            let parentB = this.getRandomMember(totalFitness);
+            while (parentA == parentB) {
+                parentA = this.getRandomMember(totalFitness);
+                parentB = this.getRandomMember(totalFitness);
+            }
+            let child;
+            child = parentA.crossover(parentB);
+            child.mutate();
+        }
     }
 
     getRandomMember(totalFitness) {
         let rand = random(0, totalFitness);
-        for (let i = 0; i < this.population.length; i++) {
+        for (let i = 0; i < this.size; i++) {
             if (this.population[i].fitness < rand) {
-                return this.population[i];     
+                return this.population[i];
             }
             rand -= this.population[i].fitness;
         }
